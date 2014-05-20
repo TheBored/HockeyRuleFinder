0eba409ceb7bd63e26fbbfa97736d6ff0a7dc8bb
Brian Maxwell <brianmxwll@gmail.com>
2014-05-20 00:26:53 +0000

    Adding basic splash screen

---
+++ HockeyRuleFinder/src/main/java/com/teebz/hrf/activities/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.teebz.hrf.activities;
+
+import com.teebz.hrf.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashScreenActivity extends HRFActivity {
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() { // This method will be executed once the timer is over
+                // Start Main
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+
+                // Close this out.
+                finish();
+
+                overridePendingTransition(R.anim.fadein, R.anim.fadeout);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
